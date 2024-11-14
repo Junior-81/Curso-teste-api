fa435e987c66c8cf6706d847c9143a95e4657b1a
Junior-81 <jab.junior81@gmail.com>
2024-11-13 23:39:19 -0300

    first commit

---
+++ src/main/java/br/com/junior/api/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.junior.api.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
